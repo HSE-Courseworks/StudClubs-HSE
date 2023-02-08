6c893a1abeabdaa5b0e893e27054e865c9e07162
Serafimchik <98609343+Serafimchik@users.noreply.github.com>
2023-02-08 13:15:05 +0300

    filling out the event registration form without adding it to the database, a FSM is made

--- utils/db_api/quick_commands.py
+++ utils/db_api/quick_commands.py
@@ -2,6 +2,7 @@ from asyncpg import UniqueViolationError
 
 from utils.db_api.db_gino import db
 from utils.db_api.schemas.user import User
+from utils.db_api.schemas.user import Event
 
 async def add_user(user_id: int, first_name: str, last_name: str, username: str, status: str ):
     try:
@@ -25,3 +26,14 @@ async def select_user(user_id):
 async def update_status(user_id, status):
     user = await select_user(user_id)
     await user.update(status=status).apply()
+
+async def add_event(event_id: int, nullified: str, succeed: str, date_event: str, time_event: str, place: str, id_clab: int, name_event: str, description_event: str, link_event: str, user_id: int, first_name: str, last_name: str, username: str ):
+    try:
+        event = Event(event_id=event_id, nullified=nullified,succeed=succeed, date_event=date_event, time_event=time_event, place=place, id_clab=id_clab, name_event=name_event, description_event=description_event, link_event=link_event, user_id=user_id, first_name=first_name, last_name=last_name, username=username )
+        await event.create()#создаём запись мероприятия в таблице
+    except UniqueViolationError:
+        print('Мероприятие не добавлено')
+
+async def select_all_events():
+    events = await Event.query.gino.all()
+    return events
--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -4,5 +4,6 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands([
         types.BotCommand('start', 'Запустить бота'),
         types.BotCommand('help', 'Помощь'),
-        types.BotCommand('profile', 'Получить данные из БД')
+        types.BotCommand('profile', 'Получить данные из БД'),
+        types.BotCommand('new_event', 'Добавить мероприятие')
     ])
