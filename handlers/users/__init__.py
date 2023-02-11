738bc1f39b113d1cd0437448b5dbf75023339c2b
Serafimchik <98609343+Serafimchik@users.noreply.github.com>
2023-02-11 12:35:26 +0300

    add work with DB (#5)
    Co-authored-by: Artem Saratovtsev <46279571+DumDereDum@users.noreply.github.com>

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,4 +1,5 @@
-from .start import dp
+#from .start import dp
+from .bot_start import dp
 from .help import dp
 
 __all__ = ['dp'] #список параметров которые можно импортировать с папки
--- loader.py
+++ loader.py
@@ -1,11 +1,13 @@
 from aiogram import Bot, Dispatcher, types
 
+from utils.db_api.db_gino import db
+
 from data import config
 
 #Создаём переменную бота где Bot(token='токен нашего бота')
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 
-
 #Создаём диспетчер
 dp = Dispatcher(bot)
 
+__all__ = ['bot', 'dp', 'db']
